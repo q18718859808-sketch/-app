c93c66af31162f2f810545f58c99ced49686e847
您的用户名 <@example.com>
2026-02-28 17:13:18 +0800

    Add Capacitor setup for Android and Action workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.medicare.app',
+  appName: 'medicare',
+  webDir: 'dist'
+};
+
+export default config;
